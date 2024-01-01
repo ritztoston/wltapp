4fa105d2c2e00efe6819c391cac45933484a3d17
Kim Toston <ritztoston@gmail.com>
2024-01-01 10:42:56 +0800

    Add user card into component

---
+++ app/components/UserCard.tsx
@@ -0,0 +1,18 @@
+import { User } from "@prisma/client";
+import { getUserFullName } from "~/utilities";
+
+export const UserCard = ({ user }: { user: User }) => (
+  <div className="flex items-center gap-x-4 py-3 text-xs font-semibold text-gray-300">
+    <img
+      className="h-8 w-8 rounded-full"
+      src={user.image}
+      alt={getUserFullName(user.firstName, user.lastName)}
+    />
+    <div className="flex flex-col gap-y-1">
+      <div aria-hidden="true" className="flex">
+        {getUserFullName(user.firstName, user.lastName)}
+      </div>
+      <div aria-hidden="true" className="flex">{user.email}</div>
+    </div>
+  </div>
+);
