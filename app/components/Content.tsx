0f20fc57e17bac3943b4afaf2f4c4c573e6b9985
Kim Toston <ritztoston@gmail.com>
2024-01-01 08:32:02 +0800

    Import notification to content

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -5,6 +5,8 @@ import { classNames } from "~/utilities";
 
 import { Breadcrumbs } from "./Breadcrumbs";
 import { Navbar } from "./Navbar";
+import { Snackbar } from "~/utilities/types";
+import Notifications from "./Notifications";
 
 // import { Notification } from "~/utilities/types";
 
@@ -15,7 +17,7 @@ interface LocalProps {
   children: ReactNode;
   title?: string;
   sidebar?: ReactNode;
-  //   notification?: Notification | null | undefined;
+  notification?: Snackbar | null;
 }
 
 export const Content = (props: LocalProps) => {
@@ -23,7 +25,7 @@ export const Content = (props: LocalProps) => {
     <div>
       <Navbar user={props.user} />
       <main className="lg:pl-72">
-        <div className={classNames(props.sidebar ? "xl:pr-96" : "", "")}>
+        <div className={classNames(props.sidebar ? "xl:pr-80" : "", "")}>
           <Breadcrumbs title={props.title} />
           <div className="px-4 py-8 sm:px-6 lg:px-20 lg:py-10">
             {props.children}
@@ -32,17 +34,18 @@ export const Content = (props: LocalProps) => {
       </main>
 
       {props.sidebar ? (
-        <aside className="fixed inset-y-0 right-0 hidden w-96 overflow-y-auto shadow-2xl px-4 py-6 sm:px-6 lg:px-8 xl:block bg-gray-800 z-20">
-          {/* Secondary column (hidden on smaller screens) */}
+        <aside className="fixed inset-y-0 right-0 hidden w-80 overflow-y-auto shadow-2xl px-4 py-6 sm:px-6 lg:px-8 xl:block bg-gray-800 z-20">
+          {props.sidebar}
         </aside>
       ) : null}
 
-      {/* {props.notification ? (
+      {props.notification ? (
         <Notifications
           title={props.notification.title}
           description={props.notification.description}
+          type={props.notification.type}
         />
-      ) : null} */}
+      ) : null}
     </div>
   );
 };
