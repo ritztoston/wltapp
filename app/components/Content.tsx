00930eb614e2b5d17d97692977f2e75af8e6cda6
Kim Toston <ritztoston@gmail.com>
2024-01-01 10:43:31 +0800

    Adjust sidebar width

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -25,7 +25,7 @@ export const Content = (props: LocalProps) => {
     <div>
       <Navbar user={props.user} />
       <main className="lg:pl-72">
-        <div className={classNames(props.sidebar ? "xl:pr-80" : "", "")}>
+        <div className={classNames(props.sidebar ? "xl:pr-64" : "", "")}>
           <Breadcrumbs title={props.title} />
           <div className="px-4 py-8 sm:px-6 lg:px-20 lg:py-10">
             {props.children}
@@ -34,7 +34,7 @@ export const Content = (props: LocalProps) => {
       </main>
 
       {props.sidebar ? (
-        <aside className="fixed inset-y-0 right-0 hidden w-80 overflow-y-auto shadow-2xl px-4 py-6 sm:px-6 lg:px-8 xl:block bg-gray-800 z-20">
+        <aside className="fixed inset-y-0 right-0 hidden w-64 overflow-y-auto shadow-2xl px-4 py-6 sm:px-6 lg:px-8 xl:block bg-gray-800 z-20">
           {props.sidebar}
         </aside>
       ) : null}
