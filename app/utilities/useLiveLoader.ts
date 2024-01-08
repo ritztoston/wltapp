b8e59e6a007762a85fcebb6f934bfcfac4885165
Kim Toston <ritztoston@gmail.com>
2024-01-08 09:10:25 +0800

    Should only revalidate if there is data

--- app/utilities/useLiveLoader.ts
+++ app/utilities/useLiveLoader.ts
@@ -13,7 +13,7 @@ export const useLiveLoader = <T>() => {
   const { revalidate } = useRevalidator();
 
   useEffect(() => {
-    revalidate();
+    if (data) revalidate();
   }, [data, revalidate]);
 
   return useLoaderData<T>();
